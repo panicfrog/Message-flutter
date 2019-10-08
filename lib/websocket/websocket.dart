ddb0e4eea4473b1dab100c2a71e706647b96cf4e
yeyongping <burnedfrog@163.com>
2019-10-08 17:12:42 +0800

    add reconnet for websocket

--- lib/websocket/websocket.dart
+++ lib/websocket/websocket.dart
@@ -6,10 +6,9 @@ import 'package:message/network/env.dart';
 import 'package:message/websocket/message.dart';
 import 'package:web_socket_channel/io.dart';
 
-enum ChatMessageType{ Text, Picture, Voice, File }
+enum ChatMessageType { Text, Picture, Voice, File }
 
 class Websocket {
-
   static final Websocket _singleton = new Websocket._internal();
 
   factory Websocket() => _singleton;
@@ -17,28 +16,37 @@ class Websocket {
 
   IOWebSocketChannel _channel;
   bool _connected = false;
-  bool get connected  => _connected;
+  bool get connected => _connected;
 
   Stream<Message> msgStream;
 
   void connect(String token) {
-    if (_connected) { return; }
-    _channel = IOWebSocketChannel.connect("ws://" + Env.host + ":8081?token=$token");
-    _connected = true;
-    msgStream = _channel.stream.map((msg) {
-      print("websocket: $msg");
-      if (msg is String) {
-        var map = JsonDecoder().convert(msg);
-        if (map is Map) {
-          Message m = Message.fromJson(map);
-          return m;
+    if (_connected) {
+      return;
+    }
+    try {
+      _channel =
+          IOWebSocketChannel.connect("ws://" + Env.host + ":8081?token=$token");
+      _connected = true;
+      msgStream = _channel.stream.map((msg) {
+        print("websocket: $msg");
+        if (msg is String) {
+          var map = JsonDecoder().convert(msg);
+          if (map is Map) {
+            Message m = Message.fromJson(map);
+            return m;
+          } else {
+            throw Exception();
+          }
         } else {
           throw Exception();
         }
-      } else {
-        throw Exception();
-      }
-    });
+      });
+    } catch (e) {
+      Future.delayed(Duration(microseconds: 500), () {
+        connect(token);
+      });
+    }
   }
 
   void disconnect() {
@@ -49,18 +57,31 @@ class Websocket {
     _channel = null;
   }
 
-  void sendMessage(String from, to, content, ChatType type, ChatMessageType msgType)  {
+  void sendMessage(
+      String from, to, content, ChatType type, ChatMessageType msgType) {
     int m = 1;
     switch (type) {
-      case ChatType.Person: m = 1; break;
-      case ChatType.Room: m = 2; break;
+      case ChatType.Person:
+        m = 1;
+        break;
+      case ChatType.Room:
+        m = 2;
+        break;
     }
     int t = 1;
     switch (msgType) {
-      case ChatMessageType.Text: t = 1; break;
-      case ChatMessageType.Picture: t = 2; break;
-      case ChatMessageType.Voice: t = 3; break;
-      case ChatMessageType.File: t = 4; break;
+      case ChatMessageType.Text:
+        t = 1;
+        break;
+      case ChatMessageType.Picture:
+        t = 2;
+        break;
+      case ChatMessageType.Voice:
+        t = 3;
+        break;
+      case ChatMessageType.File:
+        t = 4;
+        break;
     }
     Message msg = Message(
       from: from,
